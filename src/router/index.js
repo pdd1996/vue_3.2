a3fc252bea73470241b50b85094c826463284c3b
Paddy_Pan <pdd337816464@outlook.com>
2024-09-16 20:28:55 +0800

    feat(custom): 增加layout

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/login')
+  },
+  {
+    path: '/',
+    name: '/',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../layout')
   }
 ]
 
