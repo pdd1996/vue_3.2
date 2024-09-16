39caeac089ae955682a65dc975b1bc64390382c8
Paddy_Pan <pdd337816464@outlook.com>
2024-09-17 00:30:18 +0800

    feat(custom): 定时退出

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,5 +1,6 @@
 import { login as loginApi } from '@/api/login'
 import router from '@/router'
+import { setTokenTime } from '../../utils/auth'
 
 export default {
   namespaced: true,
@@ -18,12 +19,18 @@ export default {
         loginApi(userInfo).then(res => {
           console.log(res)
           commit('serToken', res.token)
+          setTokenTime()
           router.replace('/')
           resolve()
         }).catch(err => {
           reject(err)
         })
       })
+    },
+    logout({ commit }) {
+      commit('serToken', '')
+      localStorage.clear()
+      router.replace('/login')
     }
   }
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,4 @@
+export const TOKEN_TIME = 'tokenTime'
+
+export const TOKEN_TIME_VALUE = 6000
+// 2 * 60 * 60 * 1000
